81ccfe872eb4746551e38cdd2b18ebac6b24ee9b
Gyan Parkash Gautam <gyan.300@outlook.com>
2021-11-19 15:33:20 +0530

    initian commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fluent-web-components';
+  exampleTextField = 'fluent-web-components';
+
+  onClick(value : string | null) {
+    alert('Hello ' + value);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideFluentDesignSystem, allComponents } from '@fluentui/web-components';
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+provideFluentDesignSystem().register(allComponents);
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +13,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
